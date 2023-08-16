d625a89f532f6933ce744afb799bb82ab7af274a
Andrei Corbu <andreicorbu7@gmail.com>
2023-08-16 18:55:03 +0300

    Revert "modify dockerfile"
    This reverts commit 2ea227a19be00db0b95fe1e69b1b89132d051919.

--- Wheels-n-Deals/Wheels-n-Deals.API/Settings/Dependencies.cs
+++ Wheels-n-Deals/Wheels-n-Deals.API/Settings/Dependencies.cs
@@ -58,7 +58,7 @@ public class Dependencies
             options.AddPolicy("Access-Control-Allow-Origin",
                 builder =>
                 {
-                    builder.WithOrigins("http://localhost:3000")
+                    builder.WithOrigins("http://localhost:5173")
                         .AllowAnyMethod()
                         .AllowAnyHeader()
                         .AllowCredentials();
